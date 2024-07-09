11fcaeeab792db68d2ba191d1657b94a777783f7
808raf <102700868+808raf@users.noreply.github.com>
2024-07-09 12:56:53 -1000

    Add PatientForm
    Homepage now includes a patient form

---
+++ components/SubmitButton.tsx
@@ -0,0 +1,18 @@
+import { Button } from "./ui/button";
+
+interface ButtonProps {
+  isLoading: boolean;
+  className?: string;
+  children: React.ReactNode;
+}
+
+const SubmitButton = ({ isLoading, className, children }: ButtonProps) => {
+  return (
+    <Button
+      type="submit"
+      disabled={isLoading}
+      className={className ?? "shad-primary-btn"}
+    ></Button>
+  );
+};
+export default SubmitButton;
